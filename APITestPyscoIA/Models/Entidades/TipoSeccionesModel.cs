b9c5db41cd81fa5a0e5770d6bbc3ddd393359408
WilsonSalinasFlores <63874104+WilsonSalinasFlores@users.noreply.github.com>
2025-08-17 19:19:44 -0500

    add evaluador and secciones model

---
+++ APITestPyscoIA/Models/Entidades/TipoSeccionesModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APITestPyscoIA.Models.Entidades
+{
+    [Table("TipoSecciones")]
+    public class TipoSeccionesModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Descripcion { get; set; }
+        [Required]
+        public int Duracion { get; set; } 
+        public int Orden { get; set; }
+
+        public string Icono { get; set; }
+        [Required]
+        [ForeignKey("TipoTestModel")]
+        public int IdTipoTest { get; set; }
+
+        public TipoTestModel TipoTest { get; set; }
+
+    }
+}
