c0e475354c3426fd18e758ee488d992fe0c487c7
WilsonSalinasFlores <63874104+WilsonSalinasFlores@users.noreply.github.com>
2025-09-01 11:11:26 -0500

    add controller config ubicacion

---
+++ APITestPyscoIA/Controllers/Configuracion/PaisController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APITestPyscoIA.Data;
+using APITestPyscoIA.Models.Entidades;
+
+namespace APITestPyscoIA.Controllers.Configuracion
+{
+    [Route("api/config/[controller]")]
+    [ApiController]
+    public class PaisController : ControllerBase
+    {
+        private readonly DatosDbContext _context;
+
+        public PaisController(DatosDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pais
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaisModel>>> GetPaises()
+        {
+            return await _context.Paises.ToListAsync();
+        }
+
+        // GET: api/Pais/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PaisModel>> GetPaisModel(int id)
+        {
+            var paisModel = await _context.Paises.FindAsync(id);
+
+            if (paisModel == null)
+            {
+                return NotFound();
+            }
+
+            return paisModel;
+        }
+
+        // PUT: api/Pais/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaisModel(int id, PaisModel paisModel)
+        {
+            if (id != paisModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(paisModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaisModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pais
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PaisModel>> PostPaisModel(PaisModel paisModel)
+        {
+            _context.Paises.Add(paisModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPaisModel", new { id = paisModel.Id }, paisModel);
+        }
+
+        // DELETE: api/Pais/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePaisModel(int id)
+        {
+            var paisModel = await _context.Paises.FindAsync(id);
+            if (paisModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Paises.Remove(paisModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PaisModelExists(int id)
+        {
+            return _context.Paises.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ APITestPyscoIA/Controllers/Configuracion/ProvinciaController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APITestPyscoIA.Data;
+using APITestPyscoIA.Models.Entidades;
+
+namespace APITestPyscoIA.Controllers.Configuracion
+{
+    [Route("api/config/[controller]")]
+    [ApiController]
+    public class ProvinciaController : ControllerBase
+    {
+        private readonly DatosDbContext _context;
+
+        public ProvinciaController(DatosDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Provincia
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProvinciaModel>>> GetProvincias()
+        {
+            return await _context.Provincias.ToListAsync();
+        }
+
+        // GET: api/Provincia/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProvinciaModel>> GetProvinciaModel(int id)
+        {
+            var provinciaModel = await _context.Provincias.FindAsync(id);
+
+            if (provinciaModel == null)
+            {
+                return NotFound();
+            }
+
+            return provinciaModel;
+        }
+
+        // PUT: api/Provincia/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProvinciaModel(int id, ProvinciaModel provinciaModel)
+        {
+            if (id != provinciaModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(provinciaModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProvinciaModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Provincia
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProvinciaModel>> PostProvinciaModel(ProvinciaModel provinciaModel)
+        {
+            _context.Provincias.Add(provinciaModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProvinciaModel", new { id = provinciaModel.Id }, provinciaModel);
+        }
+
+        // DELETE: api/Provincia/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProvinciaModel(int id)
+        {
+            var provinciaModel = await _context.Provincias.FindAsync(id);
+            if (provinciaModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Provincias.Remove(provinciaModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProvinciaModelExists(int id)
+        {
+            return _context.Provincias.Any(e => e.Id == id);
+        }
+    }
+}
